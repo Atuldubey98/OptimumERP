a19a1b5a0b79e895b4aad0fb071ba8faf92a2933
Atuldubey98 <atuldubey017@gmail.com>
2024-02-27 19:53:39 +0530

    fix : lazu loading pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,42 +1,57 @@
 import { Route, Routes } from "react-router-dom";
-import DashboardPage from "./features/dashboard";
-import LoginPage from "./features/login";
-import OrgPage from "./features/organizations";
-import RegisterPage from "./features/register";
-import CustomersPage from "./features/customers";
-import ProductsPage from "./features/products";
-import EstimatesPage from "./features/estimates/list";
-import CreateEstimatePage from "./features/estimates/create";
-import NotFoundPage from "./features/common/NotFoundPage";
-import CreateInvoicePage from "./features/invoices/create";
-import InvoicesPage from "./features/invoices/list";
-import Reportspage from "./features/reports";
+import { Suspense, lazy } from "react";
+import { Flex, Spinner } from "@chakra-ui/react";
+const DashboardPage = lazy(() => import('./features/dashboard'));
+const LoginPage = lazy(() => import('./features/login'));
+const OrgPage = lazy(() => import('./features/organizations'));
+const RegisterPage = lazy(() => import('./features/register'));
+const CustomersPage = lazy(() => import('./features/customers'));
+const ProductsPage = lazy(() => import('./features/products'));
+const EstimatesPage = lazy(() => import('./features/estimates/list'));
+const CreateEstimatePage = lazy(() => import('./features/estimates/create'));
+const NotFoundPage = lazy(() => import('./features/common/NotFoundPage'));
+const CreateInvoicePage = lazy(() => import('./features/invoices/create'));
+const InvoicesPage = lazy(() => import('./features/invoices/list'));
+const ReportsPage = lazy(() => import('./features/reports'));
 export default function App() {
   return (
-    <Routes>
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/:orgId">
-        <Route element={<DashboardPage />} path="dashboard" />
-        <Route element={<ProductsPage />} path="products" />
-        <Route element={<CustomersPage />} path="customers" />
-        <Route path="estimates">
-          <Route element={<EstimatesPage />} path="" />
-          <Route element={<CreateEstimatePage />} path="create" />
-          <Route element={<CreateEstimatePage />} path=":quoteId/edit" />
+    <Suspense
+      fallback={
+        <Flex
+          justifyContent={"center"}
+          height={"80svh"}
+          width={"100%"}
+          alignItems={"center"}
+        >
+          <Spinner size={"xl"} />
+        </Flex>
+      }
+    >
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/:orgId">
+          <Route element={<DashboardPage />} path="dashboard" />
+          <Route element={<ProductsPage />} path="products" />
+          <Route element={<CustomersPage />} path="customers" />
+          <Route path="estimates">
+            <Route element={<EstimatesPage />} path="" />
+            <Route element={<CreateEstimatePage />} path="create" />
+            <Route element={<CreateEstimatePage />} path=":quoteId/edit" />
+          </Route>
+          <Route path="invoices">
+            <Route element={<InvoicesPage />} path="" />
+            <Route element={<CreateInvoicePage />} path="create" />
+            <Route element={<CreateInvoicePage />} path=":invoiceId/edit" />
+          </Route>
         </Route>
-        <Route path="invoices">
-          <Route element={<InvoicesPage />} path="" />
-          <Route element={<CreateInvoicePage />} path="create" />
-          <Route element={<CreateInvoicePage />} path=":invoiceId/edit" />
+        <Route path="/organizations" element={<OrgPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/reports">
+          <Route path="" element={<ReportsPage />} />
+          <Route path=":reportType" element={<ReportsPage />} />
         </Route>
-      </Route>
-      <Route path="/organizations" element={<OrgPage />} />
-      <Route path="/register" element={<RegisterPage />} />
-      <Route path="/reports">
-        <Route path="" element={<Reportspage />} />
-        <Route path=":reportType" element={<Reportspage />} />
-      </Route>
-      <Route path="*" element={<NotFoundPage />} />
-    </Routes>
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </Suspense>
   );
 }
