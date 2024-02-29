2d7c2f72c63c8b429fcf5fe5a278703e8017691a
Atuldubey98 <atuldubey017@gmail.com>
2024-03-01 00:51:04 +0530

    feature : purchase started

---
+++ backend/src/dto/purchase.dto.js
@@ -0,0 +1,23 @@
+const Joi = require("joi");
+const itemSchema = Joi.object({
+  name: Joi.string().required().label("Item name"),
+  price: Joi.number().required().label("Price"),
+  quantity: Joi.number().required().label("Quantity"),
+  code: Joi.string().allow("").optional().label("Code"),
+  um: Joi.string().default("none").label("Unit of measurement"),
+  gst: Joi.string().default("none").label("GST applicable"),
+});
+
+const purchaseInvoice = Joi.object({
+  customer: Joi.string().required().label("Customer"),
+  description: Joi.string().optional().allow("").label("Description"),
+  items: Joi.array().items(itemSchema).required().label("Invoice Items"),
+  date: Joi.date().required().label("Invoice date"),
+  invoiceNo: Joi.string().label("Invoice No.").required(),
+  status: Joi.string()
+    .default("unpaid")
+    .valid("unpaid", "paid")
+    .label("Status"),
+}).options({ stripUnknown: true });
+
+module.exports = { purchaseInvoice };
---
+++ backend/src/models/purchase.model.js
@@ -0,0 +1,103 @@
+const { Schema, Types, model } = require("mongoose");
+
+const purchaseInvoice = new Schema(
+  {
+    customer: {
+      type: Types.ObjectId,
+      required: true,
+      ref: "customer",
+    },
+    total: {
+      type: Number,
+      default: 0,
+      required: true,
+    },
+    totalTax: {
+      type: Number,
+      default: 0,
+      required: true,
+    },
+    description: {
+      type: String,
+      default: "Thanks for the business.",
+    },
+    org: {
+      type: Types.ObjectId,
+      required: true,
+      ref: "organization",
+    },
+    items: [
+      {
+        name: {
+          type: String,
+          required: true,
+        },
+        price: {
+          type: Number,
+          required: true,
+          default: 0,
+        },
+        code: {
+          type: String,
+        },
+        quantity: {
+          type: Number,
+          required: true,
+          default: 0,
+        },
+        um: {
+          type: String,
+          default: "none",
+        },
+        gst: {
+          type: String,
+          default: "none",
+        },
+      },
+    ],
+    date: {
+      type: Date,
+      default: new Date(Date.now()),
+    },
+    invoiceNo: {
+      type: String,
+      required: true,
+    },
+    createdBy: {
+      type: Types.ObjectId,
+      required: true,
+      ref: "user",
+    },
+    updatedBy: {
+      type: Types.ObjectId,
+      ref: "user",
+    },
+    status: {
+      type: String,
+      default: "sent",
+      enum: ["paid", "unpaid"],
+    },
+    financialYear: {
+      type: {
+        start: {
+          type: Date,
+          required: true,
+        },
+        end: {
+          type: Date,
+          required: true,
+        },
+      },
+      required: true,
+    },
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+);
+const Purchase = model("purchase", purchaseInvoice);
+purchaseInvoice.index({
+  description: "text",
+});
+module.exports = Purchase;
