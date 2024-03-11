ddd14157e9fb894a8ca87935aeef809b1cc8ce4d
Atuldubey98 <atuldubey017@gmail.com>
2024-03-11 17:00:34 +0530

    fix : avatar profile

--- frontend/src/features/common/main-layout/AvatarProfile.jsx
+++ frontend/src/features/common/main-layout/AvatarProfile.jsx
@@ -1,21 +1,22 @@
 import { Avatar, Flex, Hide, Text } from "@chakra-ui/react";
 import React, { useContext } from "react";
 import useAuth from "../../../hooks/useAuth";
-import SettingContext from '../../../contexts/SettingContext';
+import SettingContext from "../../../contexts/SettingContext";
 export default function AvatarProfile({ toggleProfilePopup }) {
   const { user } = useAuth();
   const settingContext = useContext(SettingContext);
-  const role = settingContext?.role || ""
+  const role = settingContext?.role || "";
   return (
     <Flex
       cursor={toggleProfilePopup ? "cursor" : "default"}
       onClick={toggleProfilePopup}
-      justifyContent={"flex-end"}
       gap={2}
       alignItems={"center"}
     >
       <Hide below="md">
-        <Text fontWeight={"bold"}>{`${user?.name} (${role.toLocaleUpperCase()})`}</Text>
+        <Text fontWeight={"bold"}>{`${
+          user?.name
+        } (${role.toLocaleUpperCase()})`}</Text>
       </Hide>
       <Avatar name={user?.name} />
     </Flex>
