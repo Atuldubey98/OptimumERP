2f775b18e4b963310ab019d9d326c4d0c61ed402
Atuldubey98 <atuldubey017@gmail.com>
2024-03-14 01:09:06 +0530

    fix : loading of bill modal

--- frontend/src/features/estimates/list/BillModal.jsx
+++ frontend/src/features/estimates/list/BillModal.jsx
@@ -8,6 +8,7 @@ import {
   ModalFooter,
   ModalHeader,
   ModalOverlay,
+  Skeleton,
 } from "@chakra-ui/react";
 import { CiSaveDown2 } from "react-icons/ci";
 import instance, { baseURL } from "../../../instance";
@@ -16,6 +17,7 @@ import useAsyncCall from "../../../hooks/useAsyncCall";
 export default function BillModal({ onClose, isOpen, bill, entity, heading }) {
   const { requestAsyncHandler } = useAsyncCall();
   const [status, setStatus] = useState("idle");
+  const [billLoadStatus, setBillLoadStatus] = useState("loading");
   const downloadBillUrl = `/api/v1/organizations/${bill.org._id}/${entity}/${bill._id}/download`;
   const onSaveBill = requestAsyncHandler(async () => {
     setStatus("loading");
@@ -30,6 +32,7 @@ export default function BillModal({ onClose, isOpen, bill, entity, heading }) {
       billPrint.close();
     };
   });
+  const isBillLoading = billLoadStatus === "idle";
   return (
     <Modal
       size={"full"}
@@ -42,22 +45,27 @@ export default function BillModal({ onClose, isOpen, bill, entity, heading }) {
         <ModalHeader>{heading}</ModalHeader>
         <ModalCloseButton />
         <ModalBody>
-          <iframe
-            style={{
-              padding : 2,
-              borderRadius : 10
-            }}
-            width={"100%"}
-            height={720}
-            src={`${baseURL}${downloadBillUrl}`}
-            frameBorder="0"
-          />
+          <Skeleton isLoaded={isBillLoading}>
+            <iframe
+              onLoad={() => {
+                setBillLoadStatus("idle");
+              }}
+              style={{
+                padding: 2,
+                borderRadius: 10,
+              }}
+              width={"100%"}
+              height={720}
+              src={`${baseURL}${downloadBillUrl}`}
+              frameBorder="0"
+            />
+          </Skeleton>
         </ModalBody>
         <ModalFooter>
           <ButtonGroup>
             <Button onClick={onClose}>Close</Button>
             <Button
-              leftIcon={<CiSaveDown2/>}
+              leftIcon={<CiSaveDown2 />}
               onClick={onSaveBill}
               colorScheme="blue"
               isLoading={status === "loading"}
