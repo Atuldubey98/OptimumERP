50428ce962ec4dc5b3006a879ea4a01f29c58440
Atuldubey98 <atuldubey017@gmail.com>
2024-03-25 19:40:24 +0530

    fix : invoices customers

--- frontend/src/features/common/sidebar/Banner.jsx
+++ frontend/src/features/common/sidebar/Banner.jsx
@@ -1,13 +1,12 @@
-import { Flex, Image, Text } from "@chakra-ui/react";
+import { Flex, Image, useColorMode } from "@chakra-ui/react";
 import React from "react";
 
 export default function Banner() {
+  const { colorMode } = useColorMode();
+  const name = colorMode === "dark" ? "white" : "black";
   return (
     <Flex justifyContent={"center"} alignItems={"center"}>
-      <Image src="/favicon.svg" width={100} objectFit={"contain"} height={150}/>
-      <Text fontSize={"3xl"} textAlign={"center"} fontWeight={"bold"}>
-        OptimumERP
-      </Text>
+      <Image src={`/favicon-${name}.svg`} width={200} objectFit={"contain"} />
     </Flex>
   );
 }
