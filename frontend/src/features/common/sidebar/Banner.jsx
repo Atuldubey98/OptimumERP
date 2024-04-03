f965a7913b0d188523030c5b54193eebc90c8c84
Atuldubey98 <atuldubey017@gmail.com>
2024-04-03 16:13:13 +0530

    fix : favicon icon

--- frontend/src/features/common/sidebar/Banner.jsx
+++ frontend/src/features/common/sidebar/Banner.jsx
@@ -1,12 +1,11 @@
-import { Flex, Image, useColorMode } from "@chakra-ui/react";
+import { Flex, Image } from "@chakra-ui/react";
 import React from "react";
 
 export default function Banner() {
-  const { colorMode } = useColorMode();
-  const name = colorMode === "dark" ? "white" : "black";
+
   return (
     <Flex justifyContent={"center"} alignItems={"center"}>
-      <Image src={`/favicon-${name}.svg`} width={200} objectFit={"contain"} />
+      <Image src={`/favicon-white.svg`} width={200} objectFit={"contain"} />
     </Flex>
   );
 }
