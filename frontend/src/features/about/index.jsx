6ea0430850eff8405b8dae8940dbd2ef1c3fc7f6
Atuldubey98 <atuldubey017@gmail.com>
2024-04-04 15:23:05 +0530

    padding admin

--- frontend/src/features/about/index.jsx
+++ frontend/src/features/about/index.jsx
@@ -1,35 +1,45 @@
 import React from "react";
 import MainLayout from "../common/main-layout";
-import { Button, Flex, Heading, Image, Link, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Flex,
+  Heading,
+  Image,
+  Link,
+  Text,
+} from "@chakra-ui/react";
 import { PiDiscordLogo } from "react-icons/pi";
 export default function AboutPage() {
   return (
     <MainLayout>
-      <Flex
-        gap={3}
-        mt={6}
-        justifyContent={"center"}
-        flexDir={"column"}
-        alignItems={"center"}
-      >
-        <Image w={"md"} src="/favicon-white.svg" />
-        <Heading textAlign={"center"}>
-          Unlock the Power of Seamless Business Operations
-        </Heading>
-        <Text>
-          Distributed by{" "}
-          <Link href="https://github.com/Atuldubey98">Atul Dubey</Link>
-        </Text>
-        <Button
-          colorScheme="teal"
-          target="_blank"
-          leftIcon={<PiDiscordLogo />}
-          href="https://discord.com/invite/FFWvQnqj"
-          as={"a"}
+      <Box p={4}>
+        <Flex
+          gap={3}
+          mt={6}
+          justifyContent={"center"}
+          flexDir={"column"}
+          alignItems={"center"}
         >
-          Join Discord
-        </Button>
-      </Flex>
+          <Image w={"md"} src="/favicon-white.svg" />
+          <Heading textAlign={"center"}>
+            Unlock the Power of Seamless Business Operations
+          </Heading>
+          <Text>
+            Distributed by{" "}
+            <Link href="https://github.com/Atuldubey98">Atul Dubey</Link>
+          </Text>
+          <Button
+            colorScheme="teal"
+            target="_blank"
+            leftIcon={<PiDiscordLogo />}
+            href="https://discord.com/invite/FFWvQnqj"
+            as={"a"}
+          >
+            Join Discord
+          </Button>
+        </Flex>
+      </Box>
     </MainLayout>
   );
 }
