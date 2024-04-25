28a845b5db0c808f115e8f272dfbec696ddbbd81
Atuldubey98 <atuldubey017@gmail.com>
2024-04-26 00:29:17 +0530

    fix : settings context

--- frontend/src/contexts/SettingContextProvider.jsx
+++ frontend/src/contexts/SettingContextProvider.jsx
@@ -2,14 +2,20 @@ import { useContext, useEffect, useState } from "react";
 import instance from "../instance";
 import AuthContext from "./AuthContext";
 import SettingContext from "./SettingContext";
+import { useLocation } from "react-router-dom";
 export default function SettingContextProvider({ children }) {
   const [setting, setSetting] = useState(null);
   const onSetSettingForOrganization = (newSetting) => setSetting(newSetting);
   const [currentOrgRole, setCurrentOrgRole] = useState("");
   const onSetCurrentOrgRole = (currentRole) => setCurrentOrgRole(currentRole);
   const userContext = useContext(AuthContext);
+  const location = useLocation();
+  const orgId = location.pathname.substring(
+    1,
+    location.pathname.indexOf("/", 1)
+  );
   const fetchSetting = async () => {
-    const storedOrgId = localStorage.getItem("organization");
+    const storedOrgId = localStorage.getItem("organization") || orgId;
     if (!storedOrgId && userContext.user) return;
     try {
       const { data } = await instance.get(
