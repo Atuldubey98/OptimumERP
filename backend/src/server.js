01981387b13e7c5b53c8c089437dd59e22e864d8
Atuldubey98 <atuldubey017@gmail.com>
2024-07-31 00:33:33 +0530

    feature : running on all clusters

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,19 +7,24 @@ const http = require("http");
 const logger = require("./logger");
 const { MONGO_URI, PORT } = require("./config");
 const { connectDatabase, closeDBConnection } = require("./services/db.service");
-
+const cluster= require("node:cluster");
+const os = require("node:os");
 connectDatabase(MONGO_URI);
-const server = http.createServer(app);
-server.listen(PORT, () => {
-  logger.info(`Server running on ${PORT}`);
-});
-
-const onHandleExitServer = async () => {
-  logger.info("SIGTERM signal received: closing HTTP server");
-  await closeDBConnection();
-  server.close(() => {
-    logger.info("HTTP Server closed");
-    process.exit(0);
+if (cluster.isPrimary) {
+  const noOfCpus = os.cpus().length;
+  for (let i = 0; i < noOfCpus; i++) cluster.fork();
+} else {
+  const server = http.createServer(app);
+  server.listen(PORT, () => {
+    logger.info(`Server running on ${PORT}`);
   });
-};
-process.on("SIGTERM", onHandleExitServer);
+  const onHandleExitServer = async () => {
+    logger.info("SIGTERM signal received: closing HTTP server");
+    await closeDBConnection();
+    server.close(() => {
+      logger.info("HTTP Server closed");
+      process.exit(0);
+    });
+  };
+  process.on("SIGTERM", onHandleExitServer);
+}
