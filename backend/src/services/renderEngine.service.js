b6180f83ab0e143b31d6cc3860bd49b7cd571663
Atuldubey98 <atuldubey017@gmail.com>
2025-01-24 20:03:19 +0530

    fix : backend load from puppetter

--- backend/src/services/renderEngine.service.js
+++ backend/src/services/renderEngine.service.js
@@ -26,10 +26,9 @@ exports.promiseQrCode = (value) => {
     });
   });
 };
-
 exports.getPdfBufferUsingHtml = async (html) => {
   const apiKey = process.env.PDF_SHIFT_API_KEY;
-  
+
   const response = await fetch("https://api.pdfshift.io/v3/convert/pdf", {
     method: "POST",
     headers: {
