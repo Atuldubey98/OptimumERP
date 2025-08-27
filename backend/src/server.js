c61be1e3cd02a04de6e3becdba66e441312c6123
Atuldubey98 <atuldubey017@gmail.com>
2025-08-27 23:18:16 +0530

    style: format code for consistency in server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,7 +7,7 @@ const http = require("http");
 const logger = require("./logger");
 const { MONGO_URI, PORT } = require("./config");
 const { connectDatabase, closeDBConnection } = require("./services/db.service");
-const cluster= require("node:cluster");
+const cluster = require("node:cluster");
 const os = require("node:os");
 connectDatabase(MONGO_URI);
 if (cluster.isPrimary) {
@@ -15,9 +15,7 @@ if (cluster.isPrimary) {
   for (let i = 0; i < noOfCpus; i++) cluster.fork();
 } else {
   const server = http.createServer(app);
-  server.listen(PORT, () => {
-    logger.info(`Server running on ${PORT}`);
-  });
+  server.listen(PORT, () => logger.info(`Server running on ${PORT}`));
   const onHandleExitServer = async () => {
     logger.info("SIGTERM signal received: closing HTTP server");
     await closeDBConnection();
