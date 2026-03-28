a1026e3d96a3ad414094499d2f1155ca2f3554ec
Atuldubey98 <atuldubey017@gmail.com>
2026-03-28 11:18:40 +0000

    feat: update avatar and logo storage paths to use default uploads directory if NETWORK_STORAGE_PATH is not set

--- backend/src/storages/avatar.storage.js
+++ backend/src/storages/avatar.storage.js
@@ -4,7 +4,7 @@ const path = require("path");
 
 const avatarStorage = multer.diskStorage({
   destination: (_, __, cb) => {
-    const dir = path.join(process.env.NETWORK_STORAGE_PATH, "avatars");
+    const dir = path.join(process.env.NETWORK_STORAGE_PATH || "./uploads", "avatars");
 
     if (!fs.existsSync(dir)) {
       fs.mkdirSync(dir, { recursive: true });
--- backend/src/storages/logo.storage.js
+++ backend/src/storages/logo.storage.js
@@ -4,7 +4,7 @@ const path = require("path");
 
 const logoStorage = multer.diskStorage({
   destination: (_, __, cb) => {
-    const dir = path.join(process.env.NETWORK_STORAGE_PATH, "logos");
+    const dir = path.join(process.env.NETWORK_STORAGE_PATH || "./uploads", "logos");
 
     if (!fs.existsSync(dir)) {
       fs.mkdirSync(dir, { recursive: true });
