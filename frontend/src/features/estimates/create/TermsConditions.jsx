a5ff50bf77dfbcb27b9fd87221dc33082cdc3ea3
Atuldubey98 <atuldubey017@gmail.com>
2026-05-10 09:56:46 +0000

    feat: enable auto-resizing for terms and conditions textarea using react-textarea-autosize

--- frontend/src/features/estimates/create/TermsConditions.jsx
+++ frontend/src/features/estimates/create/TermsConditions.jsx
@@ -4,6 +4,7 @@ import {
   FormLabel,
   Textarea,
 } from "@chakra-ui/react";
+import TextareaAutosize from "react-textarea-autosize";
 import { useTranslation } from "react-i18next";
 
 export default function TermsAndCondtions({ formik }) {
@@ -12,10 +13,14 @@ export default function TermsAndCondtions({ formik }) {
     <FormControl isInvalid={formik.errors.terms && formik.touched.terms}>
       <FormLabel>{t("quote_ui.form.terms_and_conditions")}</FormLabel>
       <Textarea
+        as={TextareaAutosize}
+        minRows={4}
+        maxRows={15}
         placeholder={t("quote_ui.form.terms_placeholder")}
         name="terms"
         onChange={formik.handleChange}
         value={formik.values.terms}
+        resize="none"
       />
       <FormErrorMessage>{formik.errors.terms}</FormErrorMessage>
     </FormControl>
