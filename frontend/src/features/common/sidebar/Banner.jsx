6416590dbd8428d1c8fd1dccb6002d3065369228
Atuldubey98 <atuldubey017@gmail.com>
2026-05-30 18:57:02 +0000

    refactor: update homePageUrl to use VITE_API_URL environment variable

--- frontend/src/features/common/sidebar/Banner.jsx
+++ frontend/src/features/common/sidebar/Banner.jsx
@@ -6,9 +6,7 @@ import {
 import React from "react";
 
 export default function Banner() {
-  const homePageUrl = import.meta.env.DEV
-    ? "http://localhost:3000"
-    : "https://www.optimumerp.biz/";
+  const homePageUrl = import.meta.env.VITE_API_URL;
   return (
     <Flex justifyContent={"center"} alignItems={"center"}>
       <ChakraLink href={homePageUrl}>
