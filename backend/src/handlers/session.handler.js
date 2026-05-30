12ec22d287670aa4178f27d5e0733f2b5c4c1766
Atuldubey98 <atuldubey017@gmail.com>
2026-05-30 09:41:27 +0000

    fix: update cookie sameSite policy to conditionally support cross-site requests in production

--- backend/src/handlers/session.handler.js
+++ backend/src/handlers/session.handler.js
@@ -2,6 +2,7 @@ const { Router } = require("express");
 const session = require("express-session");
 const { SESSION_SECRET, MONGO_URI, NODE_ENV, SESSION_AGE } = require("../config");
 const MongoStore = require("connect-mongo");
+
 const sessionOptions = {
   secret: SESSION_SECRET,
   resave: false,
@@ -12,12 +13,11 @@ const sessionOptions = {
   }),
   cookie: {
     httpOnly: true,
-    sameSite: "strict",
+    sameSite: NODE_ENV === "production" ? "none" : "lax",
     secure: NODE_ENV === "production",
     maxAge: SESSION_AGE,
   },
 };
-
 
 const sessionHandler = Router();
 
